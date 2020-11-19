c6585a634eb874efd549fca7dd35753ea9e40563
Amy Shackles <amyshackles@gmail.com>
2020-11-19 00:13:39 -0800

    Adds testing for scripts to ensure all of the codepoints match the given script

---
+++ test/utils/scripts.test.js
@@ -0,0 +1,21 @@
+const { getScripts, scripts } = require("../../utils/scripts.js");
+const { getUnicode } = require("../../utils/getUnicode.js");
+const { getScriptRanges } = require("../../utils/getRange.js");
+const expect = require("chai").expect;
+
+describe("Scripts", () => {
+  describe("should match the codepoints laid out in scripts", () => {
+    let scriptOptions = Object.keys(scripts);
+    scriptOptions.forEach((script) => {
+      it(`should match the codepoints for ${script}`, () => {
+        let regex = new RegExp(`\^\\p{Script=${script}\}\+\$`, "u");
+        console.log(getScripts(script));
+        const codepoints = getScriptRanges(getScripts(script));
+        let matches = getUnicode(codepoints);
+        const actual = regex.test(matches);
+        const expected = true;
+        expect(actual).to.equal(expected);
+      });
+    });
+  });
+});
